4c8bede417a62098a99e733c08739e87831095e4
Christopher <cgtsompa@gmail.com>
2024-10-08 18:32:50 +0100

    Minor changes

--- simple-to-do-list/Program.cs
+++ simple-to-do-list/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 namespace simple_to_do_list
 {
@@ -8,33 +9,47 @@ namespace simple_to_do_list
         {
             Console.WriteLine("Hello, World! Welcome to my simple to do list!");
 
-            Console.WriteLine("Commands: Add, Delete, Edit, View and Mark.");
-
             List<string> tasks = new List<string>();
 
-            string userInput = Console.ReadLine();
+            bool isRunning = true;
 
-            while (userInput != "End")
+            while (isRunning)
             {
+                Console.WriteLine("\nCommands: Add, Delete, Edit, View, Mark & Quit");
+
+                string userInput = Console.ReadLine();
 
                 if (userInput == "Add")
                 {
-                    int taskNumber = 1;
-
                     Console.WriteLine("Please add a new task:");
 
                     string addTask = Console.ReadLine();
 
-                    tasks.Add(taskNumber + ". " + addTask + "=Incomplete");
+                    tasks.Add(addTask);
 
+                    Console.WriteLine("Task added!");
                 }
                 else if (userInput == "Delete")
                 {
+                    Console.WriteLine("Please enter index");
 
+                    int index = Convert.ToInt32(Console.ReadLine());
+
+                    tasks.RemoveAt(index);
+
+                    Console.WriteLine("Task deleted!");
                 }
                 else if (userInput == "Edit")
                 {
+                    Console.WriteLine("Enter index you'd like to edit:");
 
+                    int index = Convert.ToInt32(Console.ReadLine());
+
+                    string editTask = Console.ReadLine();
+
+                    tasks[index] = editTask;
+
+                    Console.WriteLine("Changes made!");
                 }
                 else if (userInput == "View")
                 {
@@ -46,6 +61,10 @@ namespace simple_to_do_list
                 else if (userInput == "Mark")
                 {
 
+                }
+                else if (userInput == "Quit")
+                {
+                    isRunning = false;
                 }
                 else
                 {
